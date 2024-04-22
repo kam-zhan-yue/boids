baa3e55482312207a19feae6cfe6ce2c9d4505d4
kamzhanyue <kamzhanyue@gmail.com>
2024-04-23 01:44:04 +1000

    Finalise Sphere Visualiser

--- Assets/Systems/SpherePoints/SphereVisualiser.cs
+++ Assets/Systems/SpherePoints/SphereVisualiser.cs
@@ -1,19 +1,21 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Sirenix.Serialization;
-using TMPro;
 using UnityEngine;
 
 public class SphereVisualiser : MonoBehaviour
 {
     [SerializeField] private SpherePoint pointPrefab;
-    [SerializeField, Range(0, 500)] private int directions = 300;
     [SerializeField, Range(0, 5f)] private float ratio = BoidExtensions.GoldenRatio;
+    [SerializeField, Range(0, 500)] private int directions = 300;
     [SerializeField, Range(0, 10f)] private float radius = 2f;
-    
-    private SpherePoint[] _points = Array.Empty<SpherePoint>();
+
+    private List<SpherePoint> _points = new List<SpherePoint>();
     private Vector3[] _directions = Array.Empty<Vector3>();
+
+    private void Awake()
+    {
+        EnsureBounds();
+    }
 
     private void Update()
     {
@@ -27,16 +29,23 @@ public class SphereVisualiser : MonoBehaviour
     
     private void EnsureBounds()
     {
-        if (_points.Length != directions)
+        if (_points.Count > directions)
         {
-            for (int i = 0; i < _points.Length; ++i)
+            int numToRemove = _points.Count - directions;
+            for (int i = 0; i < numToRemove; ++i)
             {
-                DestroyImmediate(_points[i].gameObject);
+                int index = _points.Count - (i + 1);
+                Destroy(_points[index].gameObject);
+                _points.RemoveAt(index);
             }
-            _points = new SpherePoint[directions];
-            for (int i = 0; i < _points.Length; ++i)
+        }
+        else if(_points.Count < directions)
+        {
+            int numToSpawn = directions - _points.Count;
+            for (int i = 0; i < numToSpawn; ++i)
             {
-                _points[i] = Instantiate(pointPrefab);
+                SpherePoint point = Instantiate(pointPrefab);
+                _points.Add(point);
             }
         }
     }
